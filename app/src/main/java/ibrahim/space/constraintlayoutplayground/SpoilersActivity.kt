2f58f887c94876c8e1131917d93c6beacb7a193c
Ibrahim Alghailani <ibrahim.ghailani@gmail.com>
2018-03-08 18:31:07 +0300

    Solved Challenge

--- app/src/main/java/ibrahim/space/constraintlayoutplayground/SpoilersActivity.kt
+++ app/src/main/java/ibrahim/space/constraintlayoutplayground/SpoilersActivity.kt
@@ -1,13 +1,27 @@
 package ibrahim.space.constraintlayoutplayground
 
 import android.os.Bundle
+import android.support.constraint.ConstraintSet
 import android.support.v7.app.AppCompatActivity
+import android.transition.TransitionManager
+import kotlinx.android.synthetic.main.activity_spoiler.*
 
 class SpoilersActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spoiler)
 
 
+        spoilerView.setOnLongClickListener {
+            TransitionManager.beginDelayedTransition(spoilerRootView)
+            val showSpoiler = ConstraintSet()
+            with(showSpoiler) {
+                clone(this@SpoilersActivity, R.layout.activity_spoiler_show)
+                applyTo(spoilerRootView)
+            }
+
+            true
+        }
     }
 }
